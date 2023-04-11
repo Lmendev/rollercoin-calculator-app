15bfcd19284f4268179d0480d17aa4835c967806
Luis Mendoza <lmendoza.mg@gmail.com>
2023-04-11 05:57:29 -0400

    feat(admob): add admob-plus-cordova

--- scripts/android/appAndroidBeforePrepare.js
+++
@@ -1,6 +0,0 @@
-const { exec } = require("child_process");
-require('dotenv').config()
-
-module.exports = () => {
-    exec("cordova plugin add admob-plus-cordova --nosave --variable APP_ID_ANDROID=" +  process.env.APP_ID_ANDROID)
-}
