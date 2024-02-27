dd0aa701da4c109a14bfff812ab704ebaa3d7375
Luis Mendoza <lmendoza.mg@gmail.com>
2024-02-26 23:33:05 -0300

    feat(plugin_add/after): check if admob plus is contained for each installed plugin

--- hooks/plugin_add/after.js
+++ hooks/plugin_add/after.js
@@ -1,8 +1,16 @@
 const { exec } = require("child_process");
 require('dotenv').config()
 
+const isAdmobPlusCordovaPluginNotInstalled = plugins => {
+  const admobPlusCordovaPlugins = plugins.filter(plugin => plugin.includes('admob-plus-cordova'))
+
+  return !admobPlusCordovaPlugins.length
+}
+
+
 module.exports = context => {
-  if (!context.opts.plugins.includes("admob-plus-cordova")) return
+  const plugins = context.opts.plugins
+  if (isAdmobPlusCordovaPluginNotInstalled(plugins)) return
   
   if (!(context.opts.cli_variables.APP_ID_ANDROID === "ca-app-pub-xxx~yyy")) return
   
